3625341d9a6da228ea0a4cfeab3f5604ccd8dd2b
Daniel Perez <danielfigueroa@solemti.com>
2025-11-21 17:26:15 -0700

    feat: Members

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideAnimations()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',          // ✔ permitido
+        scrollPositionRestoration: 'enabled' // Ajusta si tienes navbar fija
+      })
+    ),
+    provideAnimations()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { MembersComponent } from './pages/members/members.component';
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
 
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
   { path: 'menu', component: MenuComponent },
+  { path: 'members', component: MembersComponent },
 ];
